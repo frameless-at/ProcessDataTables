c54924b5fd37532e300f67406c7fef904215b428
frameless Media <office@frameless.at>
2025-05-25 16:17:00 +0200

    Update FieldtypeOptions.column.php

--- fieldtype_templates/FieldtypeOptions.column.php
+++ fieldtype_templates/FieldtypeOptions.column.php
@@ -6,9 +6,17 @@
  * Available variable: $value
  * Uses global config: $config['optionLabelMap']
  */
-return function($value, $config = []) {
-	$map = $config['optionLabelMap'] ?? [];
-	return isset($map[$value])
-		? htmlspecialchars($map[$value])
-		: htmlspecialchars($value);
-};
+ return function($value, $config = []) {
+      if (is_iterable($value)) {
+          $out = '';
+          foreach ($value as $raw => $label) {
+              $out .= '<span class="uk-label uk-label-small" style="margin-right:0.25em;">'
+                    . htmlspecialchars($label, ENT_QUOTES, 'UTF-8')
+                    . '</span>';
+          }
+          return $out;
+      }
+      return '<span class="uk-label uk-label-small">'
+           . htmlspecialchars((string)$value, ENT_QUOTES, 'UTF-8')
+           . '</span>';
+  };
