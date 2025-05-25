4face4772399cd7d292a7142238b43987fbbde1c
frameless Media <office@frameless.at>
2025-05-25 20:42:36 +0200

    updated fieldtype templates

--- fieldtype_templates/FieldtypeOptions.column.php
+++ fieldtype_templates/FieldtypeOptions.column.php
@@ -7,16 +7,12 @@
  * Uses global config: $config['optionLabelMap']
  */
  return function($value, $config = []) {
-      if (is_iterable($value)) {
-          $out = '';
-          foreach ($value as $raw => $label) {
-              $out .= '<span class="uk-label uk-label-small" style="margin-right:0.25em;">'
-                    . htmlspecialchars($label, ENT_QUOTES, 'UTF-8')
-                    . '</span>';
-          }
-          return $out;
-      }
-      return '<span class="uk-label uk-label-small">'
-           . htmlspecialchars((string)$value, ENT_QUOTES, 'UTF-8')
-           . '</span>';
+       $out='';
+       if($value->count()){
+        foreach($value as $item)
+            $out.=$item->title;
+        }
+        else
+            $out.=$value->title;
+        return $out;    
   };
